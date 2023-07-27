adb9dbc66fa24c76b6307d3ae70c07329280ea9c
Jesse <Jdorman1289@gmail.com>
2023-07-27 16:38:48 -0500

    practice formatted strings for java

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        int thisNum = 5;
+        int thatNum = 6;
+
+
+        String myStatement = "Five plus Six = ";
+
+        System.out.printf("%s %s",myStatement, thisNum + thatNum);
+    }
+}
