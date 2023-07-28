5f365142368ea811bf5df41142c211de1e9d0dcb
Jesse Dorman <Jdorman1289@gmail.com>
2023-07-28 11:43:39 -0500

    finish syntax type var exercise for Java I

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-
-        int thisNum = 5;
-        int thatNum = 6;
-
-
-        String myStatement = "Five plus Six = ";
-
-        System.out.printf("%s %s",myStatement, thisNum + thatNum);
+        System.out.println(Double.MAX_VALUE);
     }
 }
