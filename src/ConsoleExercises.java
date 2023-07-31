935a06df955b323abc1e92daaafbcee176229f34
Jesse Dorman <Jdorman1289@gmail.com>
2023-07-31 18:24:52 -0500

    finish console io exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
 
     public static void main(String[] args) {
-        double pi = 3.14159;
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f", pi);
 
-        System.out.printf("The value of pi is approximately %.2f", pi);
-
+//        System.out.print("Enter three words: ");
+//        String firstWord = scanner.next();
+//        String secondWord = scanner.next();
+//        String thirdWord = scanner.next();
+//
+//        System.out.printf("%s%n%s%n%s", firstWord, secondWord, thirdWord);
+//        System.out.println("Enter a sentence: ");
+//        String userSentence = scanner.nextLine();
+//        System.out.println(userSentence);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Width: ");
+        long widthValue = scanner.nextLong();
+        System.out.print("Enter the Length: ");
+        long lengthValue = scanner.nextLong();
+        long area = widthValue * lengthValue;
+        long para = (lengthValue * 2) + (widthValue * 2);
+        System.out.printf("The Area of the room is: %s%nThe Perimeter is %s",area,para);
 
     }
 }
