c7ac9a35cd23ff83732d1a6f24c1b76de2d9ef72
Jesse <Jdorman1289@gmail.com>
2023-08-01 14:51:20 -0500

    finish half of exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,29 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+//        for (int i = 5; i <= 15; i += 1) {
+//            System.out.print(i);
+//        }
+
+//        int i = 2;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        }while(i <= 100);
+
+//        long l = 100;
+//        do {
+//            System.out.println(l);
+//            l -= 5;
+//        }while(l >= -10);
+
+//        long l = 2;
+//        do {
+//            System.out.println(l);
+//            l = l * l;
+//        }while(l < 100000);
+
+
+
+    }
+}
