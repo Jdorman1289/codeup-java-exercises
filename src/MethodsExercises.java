695d6f9fc96310d540abee0bffb4b1c0731a6d03
Jesse <Jdorman1289@gmail.com>
2023-08-03 09:16:52 -0500

    finish question one for MethodsExercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,8 +1,27 @@
 public class MethodsExercises {
 
+    public static long addThem(long num1, long num2) {
+        return num1 + num2;
+    }
 
+    public static long minusThem(long num1, long num2) {
+        return num1 - num2;
+    }
+
+    public static long multiplyThem(long num1, long num2) {
+        return num1 * num2;
+    }
+
+    public static long divideThem(long num1, long num2) {
+        return num1 / num2;
+    }
 
     public static void main(String[] args) {
+
+        System.out.println(addThem(4,4));
+        System.out.println(minusThem(4,4));
+        System.out.println(multiplyThem(4,4));
+        System.out.println(divideThem(4,4));
 
     }
 }
